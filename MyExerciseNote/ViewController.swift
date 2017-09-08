2935dc9f171c771030b42811a835cf6ecf8bd40f
Your Name <you@example.com>
2017-09-08 21:34:22 +0800

    Initial Commit

---
+++ MyExerciseNote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyExerciseNote
+//
+//  Created by Ma Ding on 17/9/8.
+//  Copyright © 2017年 Ma Ding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
